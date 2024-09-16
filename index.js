396849a24a8099b38d1ff1ba6f4cd5f97db28d5b
Apriyanto Ulami Setiawan <apriyantoa252@gmail.com>
2024-09-16 23:19:00 +0700

    Update NEW

--- index.js
+++ index.js
@@ -17,19 +17,68 @@ mongoose
 // contoh data sementara
 const post = [
   {
-    id: uuidv4(),
-    title: "Belajar Node.js",
-    image: "assets/images/nodejs.png",
+    id: 1,
+    title: "Node.js",
+    subtitle: "Belajar NodeJS dengan Mudah",
+    image: "/assets/images/nodejs.png",
     alt: "nodejs",
     date: "11 September 2024",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum necessitatibus ducimus consequatur suscipit quidem eaque modi. Ut esse dicta maxime animi temporibus accusamus itaque perferendis ab deleniti eveniet ex, nulla aperiam obcaecati maiores dolorum nemo quis voluptate sint soluta a inventore, sed explicabo sunt numquam. Ipsa eligendi voluptatum accusantium deleniti labore nulla, alias iste ratione aperiam libero corrupti optio veniam, totam quibusdam possimus odit blanditiis nobis consequuntur id tenetur! Odit quasi rem incidunt molestiae blanditiis? Commodi fuga quidem, reprehenderit quaerat molestias aperiam maxime, temporibus, beatae minus optio harum inventore labore illum ratione nisi officia reiciendis libero perspiciatis explicabo repudiandae. Laboriosam?",
+    body: "Node.js adalah runtime JavaScript di sisi server yang dibangun di atas mesin V8 milik Chrome. Ini memungkinkan developer untuk menjalankan JavaScript di server, sehingga memungkinkan untuk membangun aplikasi backend dengan JavaScript.",
     author: "Apriyanto",
     category: "Programming",
   },
   {
-    id: uuidv4(),
-    title: "Belajar Express.js",
-    body: "Konten artikel...",
+    id: 2,
+    title: "Express.js",
+    subtitle: "Salah Satu Library untuk Node.js yang Mudah Digunakan",
+    image: "/assets/images/express.png",
+    alt: "nodejs",
+    date: "11 September 2024",
+    body: "Express.js adalah framework web minimalis untuk Node.js. Ini sering digunakan untuk membangun aplikasi web atau API. Express.js membuat pengembangan server dengan Node.js lebih mudah dengan menyediakan fitur-fitur yang membantu menangani rute, middleware, dan respons HTTP.",
+    author: "Apriyanto",
+    category: "Programming",
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    subtitle: "Belajar JavaScript dengan Mudah dengan Fundamental",
+    image: "/assets/images/javascript.png",
+    alt: "nodejs",
+    date: "11 September 2024",
+    body: "JavaScript adalah bahasa pemrograman yang awalnya dirancang untuk pengembangan di sisi frontend (klien), tetapi sekarang juga digunakan di sisi backend (server) berkat Node.js. JavaScript adalah salah satu bahasa yang paling banyak digunakan di dunia pengembangan web dan dikenal karena kemampuannya untuk membuat halaman web lebih interaktif.",
+    author: "Apriyanto",
+    category: "Programming",
+  },
+  {
+    id: 4,
+    title: "React.JS",
+    subtitle: "Framework JavaScript Paling Populer di Indonesia",
+    image: "/assets/images/ReactJS.png",
+    alt: "nodejs",
+    date: "11 September 2024",
+    body: "React.js adalah pustaka (library) JavaScript yang digunakan untuk membangun antarmuka pengguna (User Interfaces/UI) secara komponen. React dikembangkan oleh Facebook dan telah menjadi salah satu teknologi frontend yang paling populer.",
+    author: "Apriyanto",
+    category: "Programming",
+  },
+  {
+    id: 5,
+    title: "React.JS",
+    subtitle: "Framework JavaScript Paling Populer di Indonesia",
+    image: "/assets/images/nodejs.png",
+    alt: "nodejs",
+    date: "11 September 2024",
+    body: "Konten artikelLorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, maxime.",
+    author: "Apriyanto",
+    category: "Programming",
+  },
+  {
+    id: 6,
+    title: "React.JS",
+    subtitle: "Framework JavaScript Paling Populer di Indonesia",
+    image: "/assets/images/nodejs.png",
+    alt: "nodejs",
+    date: "11 September 2024",
+    body: "Konten artikelLorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, maxime.",
     author: "Apriyanto",
     category: "Programming",
   },
@@ -54,9 +103,16 @@ const comments = [
     date: "13 September 2024",
     text: "Sangat bermanafaat oleh Devtech Hidayah Mantap",
   },
+  {
+    id: uuidv4(),
+    username: "Muhammad Ali Farizi",
+    date: "14 September 2024",
+    text: "Sangat bermanafaat oleh Devtech Hidayah Keren",
+  },
 ];
 
 // Middleware
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -64,7 +120,6 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "public")));
 // app.use((req, res) => {
 //   console.log("we Got");
 //   res.send({ message: "Hallo" });
@@ -74,17 +129,33 @@ app.get("/", (req, res) => {
   res.render("form.ejs");
 });
 
-// app.get("/index", (req, res) => {
-//   res.render("index.ejs");
-// });
+// Home Page
 
-app.post("/login", (req, res) => {
-  res.render("home.ejs");
+app
+  .get("/home", (req, res) => {
+    res.render("home.ejs");
+  })
+  .post("/home", (req, res) => {
+    res.render("home.ejs");
+  });
+
+app.get("/blog", (req, res) => {
+  res.render("blog.ejs", { post, comments });
 });
 
 app
-  .get("/blog", (req, res) => {
-    res.render("blog.ejs", { post, comments });
+  .get("/blog/:id", (req, res) => {
+    const { id } = req.params;
+    const posted = post.find((c) => c.id === parseInt(id));
+    if (posted) {
+      res.render("show.ejs", { posted });
+    } else {
+      res.send(`
+        <div style="width:100%; height:90vh; display:flex; flex-direction:column; align-items:center; justify-content:center">
+        <h1 style="color:red; text-align:center" >
+        Oops 404 <br>Page Not Found</h1>
+        </div>`);
+    }
   })
   .post("/blog", (req, res) => {
     const { username, text } = req.body;
@@ -103,11 +174,18 @@ app
   .patch("/blog", (req, res) => {})
   .delete("/blog", (req, res) => {});
 
-app.get("/blog/:id", (req, res) => {
-  const { id } = req.params;
-  const post = post.find((c) => c.id == id);
-  res.render("/blog/show", { post });
-});
+app
+  .get("/show/:id", (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find((c) => c.id === id);
+    res.render("show.ejs", { comment });
+  })
+  .patch("/show/:id", (req, res) => {
+    const newComment = req.body.text;
+    const foundComment = comments.find((c) => c.id === id);
+    foundComment.text = newComment;
+    res.redirect("show", { comment: foundComment });
+  });
 
 app.get("*", (req, res) => {
   res.send(`
